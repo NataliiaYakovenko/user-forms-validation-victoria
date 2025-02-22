dd39ad9665f57d296e8d4a6cf8aedd5f4f77a2fc
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-22 07:12:00 +0200

    Add Todo-list

--- todo-list/index.js
+++ todo-list/index.js
@@ -1,53 +1,53 @@
-"use strict";
+'use strict';
 
-const todoForm = document.querySelector(".todo-container>form");
-const todoInput = document.querySelector(".todo-container input");
-const todoList = document.querySelector(".todoList");
+const todoForm = document.querySelector('.todoContainer>form');
+const todoInput = document.querySelector('.todoContainer input');
+const todoList = document.querySelector('.todoList');
 
-// const TODO_REX_EXP = /^\s*$/;
-const TODO_REX_EXP = /^(?!\s*$).+/;
+const TODO_REX_EXP = /^\s*$/;
 
-todoInput.oninput = (e) => {
-  if (TODO_REX_EXP.test(e.target.value)) {
-    e.target.classList.add("valid");
-    e.target.classList.remove("invalid");
+todoInput.oninput = ({ target }) => {
+  if (!TODO_REX_EXP.test(target.value)) {
+    target.classList.add('valid');
+    target.classList.remove('invalid');
   } else {
-    e.target.classList.remove("valid");
-    e.target.classList.add("invalid");
+    target.classList.remove('valid');
+    target.classList.add('invalid');
   }
 };
 
-todoForm.onsubmit = (e) => {
+todoForm.onsubmit = e => {
   e.preventDefault();
-  if (TODO_REX_EXP.test(e.target.elements["todo-item"].value)) {
-    const todoListItem = createTodoItem(e.target.elements["todo-item"].value);
-    todoList.append(todoListItem);
-    e.target.elements["todo-item"].value = "";
-    e.target.elements["todo-item"].classList.remove("valid");
+  const todoItem = e.target.elements['todo-item'];
+  if (!TODO_REX_EXP.test(todoItem.value)) {
+    todoList.append(createTodoItem(todoItem.value));
+    todoItem.value = '';
+    todoItem.classList.remove('valid');
   } else {
-    e.target.elements["todo-item"].classList.add("invalid");
+    todoItem.classList.add('invalid');
   }
 };
 
 function createTodoItem(value) {
-  const todoListItem = document.createElement("li");
-  const delBtn = document.createElement("button");
-  delBtn.textContent = "X";
-  delBtn.onclick = (e) => {
-    e.target.parentElement.remove();
+  const todoListItem = document.createElement('li');
+  todoListItem.classList.add('todoListItem');
+
+  const delBtn = document.createElement('button');
+  delBtn.textContent = 'X';
+  delBtn.onclick = ({ target }) => {
+    target.parentElement.remove();
   };
 
-  const isDoneEl = document.createElement('input')
+  const isDoneEl = document.createElement('input');
   isDoneEl.type = 'checkbox';
-  isDoneEl.onchange = ({target})=>{
-    target.nextSibling.classList.toggle('doneTask')
-  }
+  isDoneEl.onchange = ({ target }) => {
+    target.nextSibling.classList.toggle('doneTask');
+  };
 
-
-  const todoValue = document.createElement("span");
+  const todoValue = document.createElement('span');
   todoValue.textContent = value;
 
-  todoListItem.append(isDoneEl,delBtn, todoValue);
+  todoListItem.append(isDoneEl, todoValue, delBtn);
 
   return todoListItem;
 }
